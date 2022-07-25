86b3d27913cb277ea93f23e56bd0c8113eca3fef
gspetillo <gspetillo@gmail.com>
2022-07-25 15:26:58 -0300

    feat: add user interface

---
+++ gabrielpetillo-d3-avaliacao/Interfaces/IUser.cs
@@ -0,0 +1,18 @@
+﻿using gabrielpetillo_d3_avaliacao.Models;
+
+namespace gabrielpetillo_d3_avaliacao.Interfaces
+{
+    /// <summary>
+    /// Interface com as operações básicas de manipulação de arquivo
+    /// </summary>
+    internal interface IUser
+    {
+        User SearchByEmail();
+
+        void Create(User User);
+
+        void Update(User User);
+
+        void Delete(string idUser);
+    }
+}
