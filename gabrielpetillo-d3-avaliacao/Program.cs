74b4cbc51659c564473a6252af433950301e9f53
gspetillo <gspetillo@gmail.com>
2022-07-25 15:10:18 -0300

    feat: add project first structure

---
+++ gabrielpetillo-d3-avaliacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace gabrielpetillo_d3_avaliacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menuAction;
+
+            Console.WriteLine("========== CTEDS D3: Login ==========\n");
+            Console.WriteLine("Selecione uma das ações abaixo:\n");
+            Console.WriteLine("1. Acessar o sistema (Login)");
+            Console.WriteLine("2. Cancelar (Exit)\n");
+            Console.Write(">> Ação: ");
+            menuAction = Console.ReadLine();
+
+            Console.Write("menu: " + menuAction);
+        }
+    }
+}
